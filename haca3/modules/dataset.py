Minor update to fix an issue in normalization

haca3/modules/dataset.py
@@ -26,7 +26,7 @@ def get_tensor_from_fpath(fpath):
         # image = image - pmin
         p99 = np.percentile(image.flatten(), 99)
         image = image / (p99+1e-5)
-        image = np.clip(image, a_min=0.0, a_max=2.0*p99)
+        image = np.clip(image, a_min=0.0, a_max=3.0)
 
         image = np.array(default_transform(image))
         image = ToTensor()(image)

haca3/modules/network.py
@@ -207,7 +207,6 @@ class ThetaEncoder(nn.Module):
 class AttentionModule(nn.Module):
     def __init__(self, dim, v_ch=5):
         super().__init__()
-        self.temperature = 1.0
         self.dim = dim
         self.v_ch = v_ch
         self.q_fc = nn.Sequential(
@@ -223,7 +222,7 @@ class AttentionModule(nn.Module):
 
         self.scale = self.dim ** (-0.5)
 
-    def forward(self, q, k, v, modality_dropout=None):
+    def forward(self, q, k, v, modality_dropout=None, temperature=1.0):
         """
         Attention module for optimal anatomy fusion.
 
@@ -269,7 +268,7 @@ class AttentionModule(nn.Module):
             modality_dropout = modality_dropout.view(batch_size, num_contrasts, 1, 1).permute(0, 2, 3, 1)
             dot_prod_interp = dot_prod_interp - (modality_dropout.repeat(1, image_dim, image_dim, 1).detach() * 1e5)
 
-        attention = (dot_prod_interp / self.temperature).softmax(dim=-1)
+        attention = (dot_prod_interp / temperature).softmax(dim=-1)
         v = attention.view(batch_size, num_v_patches, 1, num_contrasts) @ v
         v = v.view(batch_size, image_dim, image_dim, self.v_ch).permute(0, 3, 1, 2)
         attention = attention.view(batch_size, image_dim, image_dim, num_contrasts).permute(0, 3, 1, 2)

haca3/modules/utils.py
@@ -57,7 +57,7 @@ def dropout_contrasts(available_contrast_id, contrast_id_to_drop=None):
         available_contrast_ids_per_subject = (available_contrast_id[i] == 1).nonzero(as_tuple=False).squeeze(1)
         num_available_contrasts = available_contrast_ids_per_subject.numel()
         if num_available_contrasts > 1:
-            num_contrast_to_drop = torch.randperm(num_available_contrasts-1)[0]
+            num_contrast_to_drop = torch.randperm(num_available_contrasts - 1)[0]
             contrast_ids_to_drop = torch.randperm(num_available_contrasts)[:num_contrast_to_drop]
             contrast_ids_to_drop = available_contrast_ids_per_subject[contrast_ids_to_drop]
             contrast_id_after_dropout[i, contrast_ids_to_drop] = 0.0
@@ -147,3 +147,21 @@ def normalize_intensity(image):
     # image = np.clip(image, a_min=0.0, a_max=p99)
     image = image / p99
     return image, p99
+
+
+def zero_pad(image, image_dim=256):
+    [n_row, n_col, n_slc] = image.shape
+    image_padded = np.zeros((image_dim, image_dim, image_dim))
+    center_loc = image_dim // 2
+    image_padded[center_loc - n_row // 2: center_loc + n_row - n_row // 2,
+                 center_loc - n_col // 2: center_loc + n_col - n_col // 2,
+                 center_loc - n_slc // 2: center_loc + n_slc - n_slc // 2] = image
+    return image_padded
+
+
+def crop(image, n_row, n_col, n_slc):
+    image_dim = image.shape[0]
+    center_loc = image_dim // 2
+    return image[center_loc - n_row // 2: center_loc + n_row - n_row // 2,
+                 center_loc - n_col // 2: center_loc + n_col - n_col // 2,
+                 center_loc - n_slc // 2: center_loc + n_slc - n_slc // 2]

haca3/test.py
@@ -7,16 +7,31 @@ import numpy as np
 import torch
 from torchvision.transforms import ToTensor
 
+from skimage.filters import threshold_otsu
+from skimage.morphology import isotropic_closing
+
 from .modules.model import HACA3
-from .modules.utils import mkdir_p
+from .modules.utils import *
+
+
+def background_removal(image_vol):
+    [n_row, n_col, n_slc] = image_vol.shape
+    thresh = threshold_otsu(image_vol)
+    mask = (image_vol >= thresh) * 1.0
+    mask = zero_pad(mask, 256)
+    mask = isotropic_closing(mask, radius=20)
+    mask = crop(mask, n_row, n_col, n_slc)
+    image_vol[mask < 1e-4] = 0.0
+    return image_vol
 
 
 def obtain_single_image(image_path):
     image_obj = nib.Nifti1Image.from_filename(image_path)
     image_vol = np.array(image_obj.get_fdata().astype(np.float32))
     thresh = np.percentile(image_vol.flatten(), 99)
-    image_vol = np.clip(image_vol, a_min=0.0, a_max=thresh)
     image_vol = image_vol / thresh
+    image_vol = np.clip(image_vol, a_min=0.0, a_max=3.0)
+    image_vol = background_removal(image_vol)
 
     n_row, n_col, n_slc = image_vol.shape
     # zero padding